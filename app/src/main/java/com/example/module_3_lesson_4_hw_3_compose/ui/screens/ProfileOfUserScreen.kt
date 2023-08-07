2f033e003f6245b7ba731091cd5c48ca2e162e70
VD <vdfree448@protonmail.com>
2023-08-07 12:49:21 +0300

    added CircularProgressIndicator to profile screen and repos screen

--- app/src/main/java/com/example/module_3_lesson_4_hw_3_compose/ui/screens/ProfileOfUserScreen.kt
+++ app/src/main/java/com/example/module_3_lesson_4_hw_3_compose/ui/screens/ProfileOfUserScreen.kt
@@ -1,6 +1,7 @@
 package com.example.module_3_lesson_4_hw_3_compose.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -9,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,88 +40,100 @@ fun ProfileOfUserScreen(
     val profileUiState by appViewModel.profileUiState.collectAsState()
     val uriHandler = LocalUriHandler.current
 
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(
-                all = dimensionResource(id = R.dimen.padding_small)
-            ),
-        shape = RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)),
-        elevation = CardDefaults.cardElevation(
-            dimensionResource(id = R.dimen.padding_xsmall)
-        ),
-        colors = CardDefaults.cardColors(Black10)
-    ) {
+    if (profileUiState.currentUser.login == "1") {
         Column(
-            modifier = Modifier
-                .padding(all = dimensionResource(id = R.dimen.padding_medium))
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            AsyncImage(
-                model = profileUiState.currentUser.avatar_url,
-                contentDescription = "imageOnProfileScreen",
-                modifier = Modifier
-                    .clip(RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)))
-                    .align(Alignment.CenterHorizontally)
+            CircularProgressIndicator(
+                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_medium))
             )
-            Text(
+        }
+    } else {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(
+                    all = dimensionResource(id = R.dimen.padding_small)
+                ),
+            shape = RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)),
+            elevation = CardDefaults.cardElevation(
+                dimensionResource(id = R.dimen.padding_xsmall)
+            ),
+            colors = CardDefaults.cardColors(Black10)
+        ) {
+            Column(
                 modifier = Modifier
-                    .padding(top = dimensionResource(id = R.dimen.padding_medium))
-                    .align(Alignment.CenterHorizontally),
-                text = profileUiState.currentUser.login,
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-
-            Divider(
-                thickness = dimensionResource(id = R.dimen.thickness_divider),
-                modifier = Modifier
-                    .padding(top = dimensionResource(id = R.dimen.padding_medium))
-            )
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = dimensionResource(id = R.dimen.padding_medium)
-                    ),
-                onClick = {
-                    uriHandler.openUri(profileUiState.currentUser.html_url)
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Purple40,
-                    contentColor = Color.White,
-                )
+                    .padding(all = dimensionResource(id = R.dimen.padding_medium))
             ) {
-                Text(
-                    text = stringResource(id = R.string.profileLink),
-                    fontSize = 16.sp
+
+                AsyncImage(
+                    model = profileUiState.currentUser.avatar_url,
+                    contentDescription = "imageOnProfileScreen",
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)))
+                        .align(Alignment.CenterHorizontally)
                 )
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        top = dimensionResource(id = R.dimen.padding_small),
-                        bottom = dimensionResource(id = R.dimen.padding_small)
-                    ),
-                onClick = {
-                    appViewModel.usersRepositories(
-                        user = profileUiState.currentUser.login
+                Text(
+                    modifier = Modifier
+                        .padding(top = dimensionResource(id = R.dimen.padding_medium))
+                        .align(Alignment.CenterHorizontally),
+                    text = profileUiState.currentUser.login,
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Divider(
+                    thickness = dimensionResource(id = R.dimen.thickness_divider),
+                    modifier = Modifier
+                        .padding(top = dimensionResource(id = R.dimen.padding_medium))
+                )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.padding_medium)
+                        ),
+                    onClick = {
+                        uriHandler.openUri(profileUiState.currentUser.html_url)
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Purple40,
+                        contentColor = Color.White,
                     )
-                    onRepositoriesClicked()
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Purple40,
-                    contentColor = Color.White,
-                )
-            ) {
-                Text(
-                    text = stringResource(id = R.string.button_repositories),
-                    fontSize = 16.sp
-                )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.profileLink),
+                        fontSize = 16.sp
+                    )
+                }
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = dimensionResource(id = R.dimen.padding_small),
+                            bottom = dimensionResource(id = R.dimen.padding_small)
+                        ),
+                    onClick = {
+                        appViewModel.usersRepositories(
+                            user = profileUiState.currentUser.login
+                        )
+                        onRepositoriesClicked()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Purple40,
+                        contentColor = Color.White,
+                    )
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.button_repositories),
+                        fontSize = 16.sp
+                    )
+                }
             }
         }
     }
+
 }
--- app/src/main/java/com/example/module_3_lesson_4_hw_3_compose/ui/screens/RepositoriesOfUserScreen.kt
+++ app/src/main/java/com/example/module_3_lesson_4_hw_3_compose/ui/screens/RepositoriesOfUserScreen.kt
@@ -1,8 +1,10 @@
 package com.example.module_3_lesson_4_hw_3_compose.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -12,6 +14,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -40,45 +43,35 @@ fun RepositoriesOfUserScreen(
     val reposUiState by appViewModel.reposUiState.collectAsState()
     val usersRepositories = reposUiState.repositoriesOfUser
 
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(
-                all = dimensionResource(id = R.dimen.padding_small)
-            ),
-        shape = RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)),
-        elevation = CardDefaults.cardElevation(
-            dimensionResource(id = R.dimen.padding_xsmall)
-        ),
-        colors = CardDefaults.cardColors(Black10)
-    ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 16.dp),
+    if (reposUiState.loginOfUser == "") {
+        Column(
+            modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            item {
-                Text(
-                    modifier = Modifier
-                        .padding(
-                            start = dimensionResource(id = R.dimen.padding_medium),
-                            end = dimensionResource(id = R.dimen.padding_medium),
-                            top = dimensionResource(id = R.dimen.padding_small),
-                            bottom = dimensionResource(id = R.dimen.padding_medium),
-                        ),
-                    text = stringResource(
-                        id = R.string.text_repositories_of_user,
-                        reposUiState.loginOfUser
-                    ),
-                    textAlign = TextAlign.Center,
-                    color = Color.White,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            if (usersRepositories.size == 0) {
+            CircularProgressIndicator(
+                modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    } else {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(
+                    all = dimensionResource(id = R.dimen.padding_small)
+                ),
+            shape = RoundedCornerShape(dimensionResource(id = R.dimen.padding_medium)),
+            elevation = CardDefaults.cardElevation(
+                dimensionResource(id = R.dimen.padding_xsmall)
+            ),
+            colors = CardDefaults.cardColors(Black10)
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 item {
                     Text(
                         modifier = Modifier
@@ -89,47 +82,70 @@ fun RepositoriesOfUserScreen(
                                 bottom = dimensionResource(id = R.dimen.padding_medium),
                             ),
                         text = stringResource(
-                            id = R.string.repositories_is_empty
+                            id = R.string.text_repositories_of_user,
+                            reposUiState.loginOfUser
                         ),
-                        textAlign = TextAlign.Start,
+                        textAlign = TextAlign.Center,
                         color = Color.White,
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold
                     )
                 }
-            }
-            itemsIndexed(usersRepositories) { index, item ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            horizontal = dimensionResource(id = R.dimen.padding_xsmall),
-                            vertical = dimensionResource(id = R.dimen.padding_xsmall)
-                        ),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        modifier = Modifier.weight(1f),
-                        text = item.name,
-                        color = Color.White,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                    Spacer(
+                if (usersRepositories.size == 0) {
+                    item {
+                        Text(
+                            modifier = Modifier
+                                .padding(
+                                    start = dimensionResource(id = R.dimen.padding_medium),
+                                    end = dimensionResource(id = R.dimen.padding_medium),
+                                    top = dimensionResource(id = R.dimen.padding_small),
+                                    bottom = dimensionResource(id = R.dimen.padding_medium),
+                                ),
+                            text = stringResource(
+                                id = R.string.repositories_is_empty
+                            ),
+                            textAlign = TextAlign.Start,
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                itemsIndexed(usersRepositories) { index, item ->
+                    Row(
                         modifier = Modifier
-                            .width(dimensionResource(id = R.dimen.padding_small))
-                    )
-                    HyperlinkText(
-                        modifier = Modifier
-                            .padding(start = dimensionResource(id = R.dimen.padding_medium)),
-                        fullText = stringResource(id = R.string.open),
-                        hyperLinks = mutableMapOf(
-                            stringResource(id = R.string.open) to reposUiState.repositoriesOfUser[index].html_url
-                        ),
-                        linkTextColor = Pink50
-                    )
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = dimensionResource(id = R.dimen.padding_xsmall),
+                                vertical = dimensionResource(id = R.dimen.padding_xsmall)
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = item.name,
+                            color = Color.White,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Spacer(
+                            modifier = Modifier
+                                .width(dimensionResource(id = R.dimen.padding_small))
+                        )
+                        HyperlinkText(
+                            modifier = Modifier
+                                .padding(start = dimensionResource(id = R.dimen.padding_medium)),
+                            fullText = stringResource(id = R.string.open),
+                            hyperLinks = mutableMapOf(
+                                stringResource(id = R.string.open) to reposUiState.repositoriesOfUser[index].html_url
+                            ),
+                            linkTextColor = Pink50
+                        )
+                    }
                 }
             }
         }
     }
+
+
 }
